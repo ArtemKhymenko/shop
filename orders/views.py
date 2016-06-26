f511be95a7eb50400898982884ec164da93f5e4d
ArtemKhymenko <breakerhim@ua.fm>
2016-06-26 20:30:30 +0300

    Create view of order

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
-from .models import OrderItem
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 from cart.cart import Cart
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                'admin/orders/order/detail.html',
+                {'order': order})
 
 def order_create(request):
     cart = Cart(request)
